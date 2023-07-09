669cc89dbbfd7a34255fb323d77d3562cbae5869
arctenis <arctenischloria@gmail.com>
2023-07-09 18:18:24 +0200

    Add error management to password.py

--- lock/password.py
+++ lock/password.py
@@ -1,12 +1,16 @@
 import secrets
 import string
 
+MIN_PASSWORD_LENGTH = 8
+MIN_WORD_COUNT = 8
+MIN_LETTERS = 4
 
 def create_password(length: int) -> str:
-    """Create a password with at least 1 digit, 1 uppercase letter, 1 lowercase
+    """
+    Create a password with at least 1 digit, 1 uppercase letter, 1 lowercase
     letter, and 1 special character and has a minimum length of 8 characters.
     """
-    if length < 8:
+    if length < MIN_PASSWORD_LENGTH:
         raise ValueError("Password length must be at least 8 characters")
 
     symbols = "!@#$%^&*()_+-=[]{};:,./<>?"
@@ -14,12 +18,11 @@ def create_password(length: int) -> str:
 
     while True:
         password = "".join(secrets.choice(characters) for i in range(length))
-        if (any(c.islower() for c in password)
-                and any(c.isupper() for c in password)
-                and sum(c.isdigit() for c in password) >= 3
-                and any(c in symbols for c in password)):
+        try:
+            check_password_strength(password)
             break
-
+        except ValueError:
+            continue
     return password
 
 
@@ -29,15 +32,27 @@ def check_password_strength(password: str) -> bool:
     at least 1 digit, 1 uppercase letter, 1 lowercase letter,
     and 1 special character.
     """
-    valid_length = len(password) >= 8
+    valid_length = len(password) >= MIN_PASSWORD_LENGTH
     has_digit = any(char.isdigit() for char in password)
     has_upper = any(char.isupper() for char in password)
     has_lower = any(char.islower() for char in password)
     has_special = any(not char.isalnum() for char in password)
+
+    if not has_digit:
+        raise ValueError("Password must have at least 1 digit")
+    if not has_upper:
+        raise ValueError("Password must have at least 1 uppercase letter")
+    if not has_lower:
+        raise ValueError("Password must have at least 1 lowercase letter")
+    if not has_special:
+        raise ValueError("Password must have at least 1 special character")
+
     return all([valid_length, has_digit, has_upper, has_lower, has_special])
 
 def generate_passphrase(length: int) -> str:
-    """Generate a passphrase of a given length"""
+    """
+    Generate a passphrase of a given length
+    """
     try:
         with open("words.txt") as f:
             words = [word.strip() for word in f]
@@ -49,10 +64,10 @@ def generate_passphrase(length: int) -> str:
 
 def check_passphrase_strength(passphrase):
     """
-    Check if passphrase has words of 4 letters minimum and has at least 4
+    Check if passphrase has words of 4 letters minimum and has at least 8
     words.
     """
     words = passphrase.split()
-    valid_length = all(len(word) >= 4 for word in words)
-    valid_words = len(words) >= 4
+    valid_length = all(len(word) >= MIN_LETTERS for word in words)
+    valid_words = len(words) >= MIN_WORD_COUNT
     return all([valid_length, valid_words])
